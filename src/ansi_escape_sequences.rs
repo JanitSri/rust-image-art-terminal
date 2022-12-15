4ae897e0544e675003b05302ef058ad22e0cd619
Janit Sri <janits_27@hotmail.com>
2022-12-15 00:39:24 -0500

    added lib.rs, using tuple structs instead of functions for ansi esc seqs

---
+++ src/ansi_escape_sequences.rs
@@ -0,0 +1,20 @@
+use std::fmt::Display;
+
+pub const CONTROL_SEQUENCE_INTRODUCER: &str = "\x1b[";
+pub const RESET: &str = "0m";
+
+pub struct TextColorRGB(pub u8, pub u8, pub u8);
+
+impl Display for TextColorRGB {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "38;2;{};{};{}m", self.0, self.1, self.2)
+    }
+}
+
+pub struct BackgroundColorRGB(pub u8, pub u8, pub u8);
+
+impl Display for BackgroundColorRGB {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "48;2;{};{};{}m", self.0, self.1, self.2)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
 
-use std::fmt::Debug;
-use std::{env, process, fs::File};
-use std::io::{prelude::*, Error, stdout, self};
+use image_art::ansi_escape_sequences::{
+    BackgroundColorRGB, TextColorRGB, CONTROL_SEQUENCE_INTRODUCER, RESET,
+};
+use std::env;
+use std::fmt::{Debug, Display};
+use std::fs::File;
+use std::io::{stdout, Error, Write};
+use std::process;
 
-use image::{GenericImageView};
+use image::GenericImageView;
 
-const CONTROL_SEQUENCE_INTRODUCER: &str = "\x1b[";
-const RESET: &str = "0m";
+const PIXEL_IDENTIFIER: &str = "●";
 
 fn main() {
     // let image_loc = get_image_loc_env_var(env::args()).unwrap_or_else(|err| {
     //     eprintln!("ERROR: Parsing Env Args {err}");
     //     process::exit(1);
     // });
-    
+
     // let img = image::open(&image_loc).unwrap_or_else(|err| {
     //     eprintln!("ERROR: Opening Image File {err}");
     //     process::exit(1);
@@ -29,17 +33,15 @@ fn main() {
     // });
 
     // print!("\x1b[38;2;255;255;0mH\x1b[0;1;3;35me\x1b[95ml\x1b[42ml\x1b[0;41mo\x1b[0m")
-    println!("{CONTROL_SEQUENCE_INTRODUCER}{}{CONTROL_SEQUENCE_INTRODUCER}{RESET}", set_text_color_rgb(0, 0, 255, "JANIT"));
-}
 
-fn set_text_color_rgb(r: u8, g: u8, b: u8, text: &str) -> String {
-    format!("38;2;{r};{g};{b}m{text}")
+    let text_color_rgb = TextColorRGB(255, 0, 0);
+    let bg_color_rgb = BackgroundColorRGB(0, 255, 0);
+    println!("{CONTROL_SEQUENCE_INTRODUCER}{text_color_rgb}{CONTROL_SEQUENCE_INTRODUCER}{bg_color_rgb}JANIT{CONTROL_SEQUENCE_INTRODUCER}{RESET}");
 }
-
 
 fn get_image_loc_env_var(mut args: impl Iterator<Item = String>) -> Result<String, &'static str> {
     args.next();
-    
+
     let image_loc = match args.next() {
         Some(arg) => arg,
         None => return Err("Missing argument with image location"),
@@ -48,10 +50,10 @@ fn get_image_loc_env_var(mut args: impl Iterator<Item = String>) -> Result<Strin
     Ok(image_loc)
 }
 
-fn write_to_file<T>(filename: &str, contents: T) -> Result<(), Error> 
-    where 
-        T: Iterator, 
-        <T as Iterator>::Item: Debug
+fn write_to_file<T>(filename: &str, contents: T) -> Result<(), Error>
+where
+    T: Iterator,
+    <T as Iterator>::Item: Debug,
 {
     match File::create(filename) {
         Ok(mut file) => {
@@ -59,10 +61,10 @@ fn write_to_file<T>(filename: &str, contents: T) -> Result<(), Error>
                 if let Err(e) = write!(file, "{:?}\n", content) {
                     return Err(e);
                 }
-            };
-        }, 
+            }
+        }
         Err(e) => return Err(e),
     }
-    
+
     Ok(())
 }
