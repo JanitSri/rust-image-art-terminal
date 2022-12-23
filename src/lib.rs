eb19dddf5ee58894abff87a3db4864f2f5308210
Janit Sri <janits_27@hotmail.com>
2022-12-22 22:24:30 -0500

    refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,52 @@
-pub mod ansi_escape_sequences;
-pub mod env_vars;
-pub mod image;
-pub mod utils;
+mod ansi_escape_sequences;
 
-// todo: is this file needed for the mod re-exports?
+use std::fmt::Error;
+use image::{DynamicImage, ImageError, GenericImageView, imageops::FilterType};
+use ansi_escape_sequences::{CONTROL_SEQUENCE_INTRODUCER, CLEAR_SCREEN, BackgroundColorRGB, TextColorRGB, RESET};
+
+// https://en.wikipedia.org/wiki/Block_Elements
+// https://en.wikipedia.org/wiki/Braille_Patterns
+const PIXEL_IDENTIFIER: &str = "\u{2588}";
+
+#[derive(Debug)]
+pub struct Image {
+    img: DynamicImage,
+}
+
+impl Image {
+    pub fn new(image_file_loc: &str) -> Result<Self, ImageError> {
+        match image::open(image_file_loc) {
+            Ok(img) => Ok(Image { img }), 
+            Err(e) => Err(e)
+        }
+    }
+
+    pub fn image(&self) -> &DynamicImage {
+        &self.img
+    }
+
+    pub fn resize_img(self, percentage: f32) -> Result<Image, Error> {
+        println!("Resizing image by {percentage}");
+        let (width, height) = self.img.dimensions();
+        let nwidth = width as f32 * percentage;
+        let nheight = height as f32 * percentage;
+        Ok(
+            Image { img: self.img.resize(nwidth as u32, nheight as u32, FilterType::Gaussian) }
+        )
+    }
+
+    // todo: implment using braille as pixel_identifer
+    pub fn display_img(&self) {
+        println!("{CONTROL_SEQUENCE_INTRODUCER}{CLEAR_SCREEN}");
+        let bg_color_rgb = BackgroundColorRGB(0, 0, 0);
+        let (width, height) = self.img.dimensions();
+        for y in 0..height {
+            for x in 0..width {
+                let rgba = self.img.get_pixel(x, y).0;
+                let text_color_rgb = TextColorRGB(rgba[0], rgba[1], rgba[2]);
+                print!("{CONTROL_SEQUENCE_INTRODUCER}{text_color_rgb}{CONTROL_SEQUENCE_INTRODUCER}{bg_color_rgb}{PIXEL_IDENTIFIER}");
+            }
+            println!("{CONTROL_SEQUENCE_INTRODUCER}{RESET}");
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,33 @@
-use image::imageops::FilterType;
-use image::{DynamicImage, GenericImageView};
-use image_art::ansi_escape_sequences::{
-    BackgroundColorRGB, TextColorRGB, CLEAR_SCREEN, CONTROL_SEQUENCE_INTRODUCER, RESET,
-};
-use image_art::env_vars::Config;
-use image_art::utils;
+mod utils;
+mod env_vars;
+
+use image::GenericImageView;
+use image_art::Image;
 use std::env;
 use std::process;
 
-// https://en.wikipedia.org/wiki/Block_Elements
-const PIXEL_IDENTIFIER: &str = "\u{2588}";
-
 fn main() {
-    let config = Config::new(env::args()).unwrap_or_else(|err| {
+    let config = env_vars::Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("ERROR: Parsing Env Args {err}");
         process::exit(1);
     });
 
     println!("{:?}", config);
 
-    let img = image::open(&config.image_file_loc()).unwrap_or_else(|err| {
+    let img = Image::new(&config.image_file_loc()).unwrap_or_else(|err| {
         eprintln!("ERROR: Opening Image File {err}");
         process::exit(1);
     });
 
     if config.file_write() {
-        let filename = "resources/foo.txt";
+        let filename = "resources/image_rgb.txt";
         println!("Writing to file {filename}");
-        utils::write_to_file(filename, img.pixels()).unwrap_or_else(|err| {
+        utils::write_to_file(filename, img.image().pixels()).unwrap_or_else(|err| {
             eprintln!("ERROR: Write to file {filename} {err}");
             process::exit(1);
         });
     }
 
-    let resized_img = resize_img(&img, config.resize_percentage());
-    let (width, height) = resized_img.dimensions();
-
-    println!("{CONTROL_SEQUENCE_INTRODUCER}{CLEAR_SCREEN}");
-    let bg_color_rgb = BackgroundColorRGB(0, 0, 0);
-    for y in 0..height {
-        for x in 0..width {
-            let rgba = resized_img.get_pixel(x, y).0;
-            let text_color_rgb = TextColorRGB(rgba[0], rgba[1], rgba[2]);
-            print!("{CONTROL_SEQUENCE_INTRODUCER}{text_color_rgb}{CONTROL_SEQUENCE_INTRODUCER}{bg_color_rgb}{PIXEL_IDENTIFIER}");
-        }
-        println!("{CONTROL_SEQUENCE_INTRODUCER}{RESET}");
-    }
-}
-
-fn resize_img(img: &image::DynamicImage, percentage: f32) -> DynamicImage {
-    println!("Resizing image by {percentage}");
-    let (width, height) = img.dimensions();
-    let nwidth = width as f32 * percentage;
-    let nheight = height as f32 * percentage;
-    img.resize(nwidth as u32, nheight as u32, FilterType::Gaussian)
+    let resized_img = img.resize_img(config.resize_percentage()).expect("Did not expect an error here on resizing");
+    resized_img.display_img();
 }
